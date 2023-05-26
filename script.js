735800dc707714af7069c827501bc19b9a3ec6d5
vanshu-himself <124697864+vanshu-himself@users.noreply.github.com>
2023-05-26 15:02:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,11 +5,11 @@
   button.addEventListener('click',()=>{
     console.log(x);
       if(x.value!=""){
-      document.getElementById('url').innerHTML=`https://localhost:8080?name=${x.value}`}
+      document.getElementById('url').innerHTML=`https://localhost:8080/?name=${x.value}`}
 	  if(y.value!=""){
-          document.getElementById('url').innerHTML=`https://localhost:8080?year=${y.value}`}
+          document.getElementById('url').innerHTML=`https://localhost:8080/?year=${y.value}`}
       if(y.value!="" && x.value!=""){
-        document.getElementById('url').innerHTML=`https://localhost:8080?name=${x.value}&year=${y.value}`}
+        document.getElementById('url').innerHTML=`https://localhost:8080/?name=${x.value}&year=${y.value}`}
        
       }
   ); 
