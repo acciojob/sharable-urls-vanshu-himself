a43c8504d29ec0d2d110830053653ae9aae5eb18
vanshu-himself <124697864+vanshu-himself@users.noreply.github.com>
2023-05-26 14:52:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 // your code here
+  const x=document.getElementById('name');
+  const y=document.getElementById('year');
+  const button=document.getElementById("button");
+  button.addEventListener('click',()=>{
+    console.log(x);
+      if(x){
+      document.getElementById('url').innerHTML=`https://localhost:8080?name=${x.value}`;}
+      if(y && x){
+        document.getElementById('url').innerHTML=`https://localhost:8080?name=${x.value}&year=${y.value}`;}
+        if(y){
+          document.getElementById('url').innerHTML=`https://localhost:8080?year=${y.value}`;}
+      }
+  ); 
